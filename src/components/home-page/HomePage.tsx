61e29530efd9940472f0fbed7c60eaac1dadbb5e
Julien Perrin <julienperrin@MacBook-Pro-de-Julien.local>
2021-09-08 12:09:04 +0200

    [master] Homepage style

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -8,13 +8,10 @@ export default function HomePage() {
       <section className={"presentation first"}>
         <div className={"home-text"}>
           <h1>Breath of the Wild l'encyclopedie</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.
-            Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.
-            Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-            Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.
-            Pellentesque congue. Ut in risus volutpat libero pharetra tempor.
-            Cras vestibulum bibendum augue.
-            Praesent egestas leo in pede. Praesent blandit odio eu enim.</p>
+          <p>Depuis les temps les plus reculés, l'histoire de la famille royale d'Hyrule est intimement liée à celle du Fléau, ce monstre que l'on nomme Ganon.</p>
+          <p>Chaque fois qu'il s'abat sur Hyrule, un jeune garçon et la princesse du royaume, l'âme du Héros et le sang de la Déesse, s'allient pour restaurer la paix. En ce temps-là, la civilisation d'Hyrule était à son apogée. Le peuple d'Hyrule sut mettre ce temps à profit.</p>
+          <p>En prévision du retour de Ganon, il mobilisa tout son savoir pour élaborer des machines qui seconderaient le Héros et la princesse dans leur tâche. Et avec elles, toute une armée de Gardiens, des soldats mécaniques autonomes mus par la volonté de protéger le peuple d'Hyrule de ses ennemis.</p>
+          <p>Quand enfin le Héros terrassa le monstre d'un coup formidable de la lame purificatrice, la princesse invoqua le pouvoir sacré hérité de son sang et scella le démon.</p>
           <button>Découvrir l'histoire</button>
         </div>
         <div className={"home-image"}>
